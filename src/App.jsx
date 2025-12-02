1293891df82010fbdedd988f1e09b27e3f6bd408
Davide De Masi <demasi.dav@gmail.com>
2025-12-02 16:59:10 +0100

    prova set github

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <>
-      <h1>Le task di oggi sono:</h1>
+      <h1></h1>
       
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
