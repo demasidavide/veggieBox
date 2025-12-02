631f5345f0ba18666b72567ceba1cfd2cb411161
Davide De Masi <demasi.dav@gmail.com>
2025-12-02 16:16:35 +0100

    primo commit-set base

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+ 
+  return (
+    <>
+      <h1>Le task di oggi sono:</h1>
+      
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          I task completati oggi sono: {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
