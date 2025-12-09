99193a4b76fe170ae466a2f651d176613cf70057
demasidavide <demasi.dav@gmail.com>
2025-12-09 17:34:02 +0100

    inserito layout card

---
+++ src/components/listIngredients/CardIng.jsx
@@ -0,0 +1,12 @@
+import "./CardIng.css";
+
+export function CardIng() {
+  return (
+    <>
+      <div className="card-ing">
+        <span className="name">Uova</span>
+        <span className="number">6</span>
+      </div>
+    </>
+  );
+}
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,11 +1,40 @@
 import "./List.css";
 import { ListRecipes } from '../components/listRecipes/ListRecipes.jsx'
+import { CardIng } from "../components/listIngredients/CardIng.jsx";
 
 export default function List() {
   return (
     <>
     <div className="container-main">
+        <div className="container-recipes">
       <ListRecipes></ListRecipes>
+
+        </div>
+        <div className="container-ingredients">
+        <h2 className="title">Lista ingredienti totali</h2>
+        <div className="container-card-ing">
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        <CardIng></CardIng>
+        </div>
+        </div>
+
 
     </div>
     </>
